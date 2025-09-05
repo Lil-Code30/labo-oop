2719d5079622285bf68897ec3c619d0b6aa6224d
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-09-05 19:57:23 -0400

    added the README file

--- Program.cs
+++ Program.cs
@@ -25,11 +25,36 @@ void Exercise1()
   {
     Console.WriteLine($"{firstName} {lastName} a {userAge} years old");
   }
-  else
-  {
-    Console.WriteLine("Vous n'etes pas etudiant.");
-  }
- 
 }
 
 // Exercise1();
+/*
+ *2. Créer un programme qui demande le montant total gagné, le nombre de personne qui
+   ont gagné ce montant et affiche le montant gagné par chaque personne (pas plus que 4
+   décimales après la virgule).
+ * 
+ */
+
+void Exercise2()
+{
+  Console.WriteLine("Exercise 2");
+  Console.WriteLine(
+    "Un programme qui demande le montant total gagné, le nombre de personne qui ont gagné ce montant et affiche le montant gagné par chaque personne. ");
+
+  Console.WriteLine("Entrez le montant total gagné: ");
+  float montant = float.Parse(Console.ReadLine());
+
+  Console.WriteLine("le nombre de personne qui ont gagné ce montant: ");
+  int nOfPersons = int.Parse(Console.ReadLine());
+
+  float amountPerPerson = montant / nOfPersons;
+  
+  Console.WriteLine($"Chaque personne ont gagné : {amountPerPerson.ToString("0.0000)}");
+}
+
+Exercise2();
+
+/*
+ *3. Créer un programme qui demande la longueur et la largeur d’un rectangle et affiche son périmètre et sa surface.
+ * 
+ */
---
+++ README.md
@@ -0,0 +1,3 @@
+# LABO OOP
+
+Ce dépôt contient tous les travaux pratiques (TP) à réaliser pour le cours de Programmation Orientée Objet I (INF11107).
